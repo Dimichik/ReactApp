df3aa54d9f53a7afa386866d8521ebed4c99005a
Dmytro <dmytro.fedchyshyn@customertimes.com>
2021-09-19 22:57:40 +0300

    create empty app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [likes, setLikes] = useState(0);
+    const [inputValue, setInputValue] = useState(' ');
+
+    function decrement() {
+        setLikes(likes + 1)
+    }
+
+    function increment() {
+        setLikes(likes - 1)
+    }
+
+    return (
+        <div>
+            <h1>{likes}</h1>
+            <h1>{inputValue}</h1>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={increment}>Increment</button>
+            <input onChange={(event) => setInputValue(event.target.value)}/>
+        </div>
+    );
 }
 
 export default App;
